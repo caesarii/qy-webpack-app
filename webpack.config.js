2de19c777949c53eff079d26a12e2417dfaa7ca1
caesarii <iwangqinghe@gmail.com>
2019-05-05 10:05:22 +0800

    feat: update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
 
+console.log('path', path.resolve(__dirname, 'dist'))
+
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+    rules: [
+        {
+            test: /\.css$/,
+            use: [
+            'style-loader',
+            'css-loader'
+            ]
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                'file-loader'
+            ]
+        }
+    ]
+    }
 };
