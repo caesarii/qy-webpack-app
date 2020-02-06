acf642146b1a1decdd081b866b7ba4de1fc91cf3
qinghe <iwangqinghe@gmail.com>
2020-02-06 11:23:19 +0800

    添加 index.html 和 html-webpack-plugin

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,6 +1,7 @@
 // webapck.config.js
 // 添加 config 之后打包输出已经与默认不同了
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     // 开发模式
@@ -12,5 +13,10 @@ module.exports = {
         filename: '[name].[hash:8].js',
         // 打包后的目录
         path: path.resolve(__dirname, '../dist')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../public/index.html')
+        })
+    ]
 }
