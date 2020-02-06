97d34997028f6de143cd264e802486f778b91726
qinghe <iwangqinghe@gmail.com>
2020-02-06 11:49:53 +0800

    多个入口打包

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -7,7 +7,10 @@ module.exports = {
     // 开发模式
     mode: 'development',
     // 入口文件
-    entry: path.resolve(__dirname, '../src/main.js'),
+    entry: {
+        main: path.resolve(__dirname, '../src/main.js'),
+        header: path.resolve(__dirname, '../src/header.js')
+    },
     output: {
         // 打包后的文件名称
         filename: '[name].[hash:8].js',
@@ -16,7 +19,17 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../public/index.html')
-        })
+            template: path.resolve(__dirname, '../public/index.html'),
+            // 要处理的 HTML 文件名
+            filename: 'index.html',
+            // 与入口文件都应的模块名, 也就是上面的 entry.main
+            chunks: ['main']
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../public/header.html'),
+            filename: 'header.html',
+            // 与入口文件都应的模块名
+            chunks: ['header']
+        }),
     ]
 }
